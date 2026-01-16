8823b2b91cb77e0536de79734f9dd8f7353f9a33
Cesar Romero <cesarliws@gmail.com>
2026-01-16 10:32:45 -0300

    fix: restore images by using public folder and implement robust redirects for all languages

--- chunks/astro_C9qJjj_v.mjs
+++
@@ -1,3 +0,0 @@
-import 'piccolore';
-import './astro/server_Y2_RHZR7.mjs';
-import 'clsx';
--- chunks/modern-delphi-concepts_B7Kxs9Vm.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./modern-delphi-concepts_BOb4eNCM.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
